03cc29cde3fa563a1714c7058c0972d4d019969a
Anthony BARILLER <anthony.bariller@orange.fr>
2021-01-27 10:22:19 +0100

    class Color

---
+++ RayTracing/Color.cpp
@@ -0,0 +1,6 @@
+#include "Color.h"
+
+Color::Color(char p_r, char p_g, char p_b):
+	r(p_r), g(p_g), b(p_b)
+{
+}
---
+++ RayTracing/Color.h
@@ -0,0 +1,12 @@
+#ifndef _VECTOR3D_H_
+#define _VECTOR3D_H_
+#include <iostream>
+
+class Color
+{
+public:
+	Color(char p_r, char p_g, char p_b);
+private:
+	char r, g, b;
+};
+#endif
